16da213e916915abed6c42ca7f40ab547bb76897
Paulo Vinícius Kuss <pviniks@gmail.com>
2025-05-29 23:47:50 +0100

    start frontend

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,7 @@
-// JS para operações CRUD com Fetch API
+var idadeRange = document.querySelector("#idadeAluno"); // input range
+var idadeValor = document.querySelector("#idadeValue"); // span
+
+idadeRange.addEventListener("input", function() {
+    idadeValor.textContent = idadeRange.value;
+    console.log(idadeRange.value);    
+});
