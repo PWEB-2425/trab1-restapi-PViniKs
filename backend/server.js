115ee3be046845e83e6c7a98acf1cfe7a4fc6ab8
Paulo Vinícius Kuss <pviniks@gmail.com>
2025-05-31 21:08:09 +0100

    front

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const path = require('path')
 const app = express()
 const port = 3058
@@ -9,7 +10,6 @@ const url = 'mongodb+srv://pviniks:HE2e0Yd9D0ZsVBan@pw.1b8agc4.mongodb.net/'
 const client = new MongoClient(url)
 const dbName = 'trab1'
 let alunosCollection
-let cursosCollection
 
 app.use(cors({ origin: "*" }))
 app.use(express.json())
@@ -20,7 +20,6 @@ async function conectarMongo() {
     await client.connect()
     const db = client.db(dbName)
     alunosCollection = db.collection('alunos')
-    cursosCollection = db.collection('cursos')
     console.log('Conectado ao MongoDB')
 }
 
@@ -44,12 +43,27 @@ app.get('/alunos', async (req, res) => {
     }
 })
 
-app.get('/cursos', async (req, res) => {
+app.post('/alunos', async (req, res) => {
     try {
-        const cursos = await cursosCollection.find().toArray()
-        res.json(cursos)
+        const aluno = req.body
+        const result = await alunosCollection.insertOne(aluno)
+        res.status(201).json({ _id: result.insertedId, ...aluno })
     } catch (error) {
-        res.status(500).json({ error: 'Erro ao buscar cursos' })
+        res.status(500).json({ error: 'Erro ao adicionar aluno' })
+    }
+})
+
+app.delete('/alunos/:id', async (req, res) => {
+    const id = req.params.id
+    try {
+        const result = await alunosCollection.deleteOne({ _id: new ObjectId(id) })
+        if (result.deletedCount === 1) {
+            res.status(200).json({ message: 'Aluno deletado com sucesso' })
+        } else {
+            res.status(404).json({ error: 'Aluno não encontrado' })
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao deletar aluno' })
     }
 })
 
